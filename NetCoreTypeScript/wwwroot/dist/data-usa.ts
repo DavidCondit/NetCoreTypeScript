e02f8a167cd861b63509e770e9b6456e8ec2a1d8
David Condit <davidcondit.ks@gmail.com>
2021-10-25 21:21:32 -0500

    Added basic api query which renders result to data-usa.html page.

---
+++ NetCoreTypeScript/wwwroot/dist/data-usa.ts
@@ -0,0 +1,16 @@
+function numberWithCommas(x: number) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+fetch('https://datausa.io/api/data?drilldowns=Nation&measures=Population')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        for (var i = 0; i < data.data.length; i++) {
+            let dataItem = data.data[i];
+            let body = document.getElementsByTagName('body')[0] as HTMLBodyElement;
+            let p = document.createElement('p');
+            p.innerHTML = `${dataItem.Year} - ${numberWithCommas(dataItem.Population)}`;
+            body.appendChild(p);
+        }
+    });
